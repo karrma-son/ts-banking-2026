f4b3ef10bcf6f95e4e8dc82b1e6ada0f38146322
Mason Karr <karr.mason@gmail.com>
2026-05-22 13:31:20 -0500

    - Cleaned up User class guard clauses and conditionals

--- User.ts
+++ User.ts
@@ -1,26 +1,23 @@
 class User {
     constructor(
         public username: string,
-        private password: any,
+        private password: string,
         private balance: number = 0,
         private loggedIn: boolean = false,
         private isRegistered: boolean = false,
-    ){};
+    ) { };
 
     register(): string {
-        
-        if (this.isRegistered = true){
-            console.log(`${this.username} is already registered`)
-            
-        }
-        if (this.isRegistered = false)
-        this.username;
-        this.password;
-        this.balance += 1000
-        this.loggedIn = true;
-        this.isRegistered = true;
 
-        
+        if (this.isRegistered == true) {
+            console.log(`${this.username} is already registered`)
+
+        } else {
+            this.balance += 1000;
+            this.loggedIn = true;
+            this.isRegistered = true;
+        }
+
         return (`
             ${this.username} 
             has signed up with, 
@@ -30,85 +27,97 @@ class User {
         )
     }
 
-        deposit(depositedAmount: number) {
-            if (this.loggedIn == true ){
-                if(depositedAmount > 0) {
-                    this.balance += depositedAmount
-                    return this.balance;
-                }
-            } if ( this.loggedIn == false) { 
-               console.log('Cannot deposit if not signed in ');
-            } else if (depositedAmount <= 0){
-               console.log('Cannot deposit 0 dollars or less')
-            }
-    
+
+    deposit(depositedAmount: number) {
+        if (this.loggedIn == false) {
+            console.log('Cannot deposit if not signed in ');
+            return;
+        }
+        else if (depositedAmount <= 0) {
+            console.log('Cannot deposit 0 dollars or less')
+            return;
         }
 
-        withdraw(withdrawAmount: number) {
-            if (this.loggedIn == true){
-                if ( withdrawAmount <= this.balance){
-                    if (withdrawAmount > 0 ){
-                        this.balance -= withdrawAmount
-                    }
-                }
-                return this.balance
-            }            
-              
-              if (this.loggedIn = true && withdrawAmount > this.balance){
-                console.log('Withdrawal ammount is more than total balance'); 
-            } else {
-               console.log('Cannot withdraw if not signed in') 
-            }
-    
-        }
-         // set guard clauses
-        // set overdraft conditionals 
 
-        getBalance(){
-            if(this.loggedIn == true)
-            return this.balance 
+        this.balance += depositedAmount
+        return this.balance;
+
+
+    }
+
+    withdraw(withdrawAmount: number) {
+        if (this.loggedIn == false) {
+            console.log('Cannot withdraw if not signed in')
+            return this.balance;                                             //user must be signed in to see balance?
+        } else if (withdrawAmount > this.balance) {
+            console.log('Withdrawal ammount is more than total balance');
+            return this.balance;
+        } else if (withdrawAmount == 0) {
+            console.log("Cannot withdraw $0")
+
         }
 
-        login(username:string, password:any){
-            if(this.loggedIn == false)
-                if(username === this.username)
-                    if(password === this.password) {
-                        this.loggedIn = true
-                    } else { 
-                        this.loggedIn = false  // alert?
-                    }
-            return ( `${this.username} is logged in: ${this.loggedIn}`)
-            
-        }
+        this.balance -= withdrawAmount
+        return this.balance
+    }
+    // set guard clauses
+    // set overdraft conditionals 
 
-        logout(){
-            this.loggedIn == true ? this.loggedIn = false: this.loggedIn = true  // true??
-            return ( `${this.username} is logged in: ${this.loggedIn}`)
-        
+    getBalance() {
+        if (this.loggedIn == true) {
+            return this.balance
         }
-        
-        display(){
-            if (this.loggedIn == false) {
+        return 0;
+    }
+
+    login(username: string, password: string) {
+        if (this.loggedIn == true) {
+            console.log("User already logged in")
+            return this.loggedIn;
+        }
+        this.loggedIn = username === this.username && password === this.password;
+
+        if (this.loggedIn == true) {
+            console.log(`${this.username} is logged in: ${this.loggedIn}`)
+        }
+        return this.loggedIn;
+
+    }
+
+    logout() {
+        this.loggedIn == true ? this.loggedIn = false : this.loggedIn = true  // true??
+        return (`${this.username} is logged in: ${this.loggedIn}`)
+
+    }
+
+    display() {
+        if (this.loggedIn == false) {
             return ("Must be signed in to display")
-               
-            } else { 
-             return `${this.username} ${this.balance} ${this.loggedIn} `
-            }
+
+        } else {
+            return `${this.username} ${this.balance} ${this.loggedIn} `
         }
+    }
 }
 
 
 const newUser1 = new User("Mason", "ASFASFA", 1230)
 
-console.log(newUser1.register());
+// console.log(newUser1.register());
 console.log(newUser1.getBalance())
-console.log(newUser1.deposit(2000))
-console.log(newUser1.withdraw(550))
-console.log(newUser1.withdraw(6550))
-console.log(newUser1.logout());
-console.log(newUser1.deposit(2000))
+// console.log(newUser1.deposit(2000));
+// newUser1.logout();
+// console.log(newUser1.withdraw(0));
+// console.log(newUser1.logout());
+// (newUser1.withdraw(550));
+//  console.log(newUser1.display());
+
+// (newUser1.deposit(2000))
 console.log(newUser1.login("Mason", "AggsFA"))
 console.log(newUser1.login("Mason", "ASFASFA"))
-console.log(newUser1.display())
-console.log(newUser1.logout());
-console.log(newUser1.display())
+// console.log(newUser1.display())
+// console.log(newUser1.logout());
+//
+
+
+//port logic to bank class
