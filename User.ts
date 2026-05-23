4f46aae0481f2077df340a3d0497c178eb3955e0
Mason Karr <karr.mason@gmail.com>
2026-05-23 11:56:51 -0500

    - Made getters and setters for User class

--- User.ts
+++ User.ts
@@ -1,123 +1,83 @@
 class User {
     constructor(
-        public username: string,
+        private username: string,
         private password: string,
+        private firstName: string,
+        private lastName: string,
+        private accountNumber: string,
         private balance: number = 0,
-        private loggedIn: boolean = false,
+        private isLoggedIn: boolean = false,
         private isRegistered: boolean = false,
+
     ) { };
 
-    register(): string {
-
-        if (this.isRegistered == true) {
-            console.log(`${this.username} is already registered`)
-
-        } else {
-            this.balance += 1000;
-            this.loggedIn = true;
-            this.isRegistered = true;
-        }
-
-        return (`
-            ${this.username} 
-            has signed up with, 
-            and a new balance of: $${this.balance}
-            and now logged in?: ${this.loggedIn}
-            and registered?: ${this.isRegistered}`
-        )
+    public set setUserName(username: string) {
+        this.username = username;
     }
 
+    public get getUserName() {
+        return this.username;
+    }
 
-    deposit(depositedAmount: number) {
-        if (this.loggedIn == false) {
-            console.log('Cannot deposit if not signed in ');
-            return;
-        }
-        else if (depositedAmount <= 0) {
-            console.log('Cannot deposit 0 dollars or less')
-            return;
-        }
+    public set setPassword(password: string) {
+        this.password = password;
+    }
 
+    public get getPassword() {
+        return this.password;
+    }
 
-        this.balance += depositedAmount
+    public set setFirstName(firstName: string) {
+        this.firstName = firstName;
+    }
+
+    public get getFirstName() {
+        return this.firstName;
+    }
+
+    public set setLastName(lastName: string) {
+        this.lastName = lastName;
+    }
+
+    public get getLastName() {
+        return this.lastName;
+    }
+
+    public get getFullName() {
+        return `${this.firstName}  ${this.lastName}`;
+    }
+
+    public set setAccountNumber(accountNumber: string) {
+        this.accountNumber = accountNumber;
+    }
+
+    public get getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public set setBalance(balance: number) {
+        this.balance = balance;
+    }
+
+    public get getBalance() {
         return this.balance;
-
-
     }
 
-    withdraw(withdrawAmount: number) {
-        if (this.loggedIn == false) {
-            console.log('Cannot withdraw if not signed in')
-            return this.balance;                                             //user must be signed in to see balance?
-        } else if (withdrawAmount > this.balance) {
-            console.log('Withdrawal ammount is more than total balance');
-            return this.balance;
-        } else if (withdrawAmount == 0) {
-            console.log("Cannot withdraw $0")
-
-        }
-
-        this.balance -= withdrawAmount
-        return this.balance
-    }
-    // set guard clauses
-    // set overdraft conditionals 
-
-    getBalance() {
-        if (this.loggedIn == true) {
-            return this.balance
-        }
-        return 0;
+    public set setIsLoggedIn(isLoggedIn: boolean) {
+        this.isLoggedIn = isLoggedIn;
     }
 
-    login(username: string, password: string) {
-        if (this.loggedIn == true) {
-            console.log("User already logged in")
-            return this.loggedIn;
-        }
-        this.loggedIn = username === this.username && password === this.password;
-
-        if (this.loggedIn == true) {
-            console.log(`${this.username} is logged in: ${this.loggedIn}`)
-        }
-        return this.loggedIn;
-
+    public get getIsLoggedIn() {
+        return this.isLoggedIn;
     }
 
-    logout() {
-        this.loggedIn == true ? this.loggedIn = false : this.loggedIn = true  // true??
-        return (`${this.username} is logged in: ${this.loggedIn}`)
-
+    public set setIsRegistered(isRegistered: boolean) {
+        this.isRegistered = isRegistered;
     }
 
-    display() {
-        if (this.loggedIn == false) {
-            return ("Must be signed in to display")
-
-        } else {
-            return `${this.username} ${this.balance} ${this.loggedIn} `
-        }
+    public get getIsRegistered() {
+        return this.isRegistered;
     }
+
 }
-
-
-const newUser1 = new User("Mason", "ASFASFA", 1230)
-
-// console.log(newUser1.register());
-console.log(newUser1.getBalance())
-// console.log(newUser1.deposit(2000));
-// newUser1.logout();
-// console.log(newUser1.withdraw(0));
-// console.log(newUser1.logout());
-// (newUser1.withdraw(550));
-//  console.log(newUser1.display());
-
-// (newUser1.deposit(2000))
-console.log(newUser1.login("Mason", "AggsFA"))
-console.log(newUser1.login("Mason", "ASFASFA"))
-// console.log(newUser1.display())
-// console.log(newUser1.logout());
-//
-
-
-//port logic to bank class
+// const newUser1 = new User("Mason", "ASFASFA", 1230)
