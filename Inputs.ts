dec638e4bae76a0b15c8e68523e589ee5578d535
Mason Karr <karr.mason@gmail.com>
2026-05-29 11:46:11 -0500

    getValueInput function

--- Inputs.ts
+++ Inputs.ts
@@ -1,3 +1,4 @@
+// import readlineSync from 'readline-sync';
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 import { BankMenu } from './BankMenu';
@@ -21,73 +22,116 @@ class Input {
                 console.log("Invalid choice")
             }
         }
-        
+
         return answer;              // input answers from these menus                                                     
     }
 
- 
+
     async userMenuInput() {
 
         let answer = ""
         let incorrectAnswer = answer !== "A" && answer !== "B" && answer !== "C" && answer !== "D"
 
-        while(incorrectAnswer){
+        while (incorrectAnswer) {
             this.bankMenu.userMenu();
             answer = (await this.rl.question("Please make a choice: ")).toUpperCase();
             incorrectAnswer = answer !== "A" && answer !== "B" && answer !== "C" && answer !== "D"
 
-            if(incorrectAnswer){
+            if (incorrectAnswer) {
                 console.log("Invalid choice")
             }
         }
         return answer;
-    
+
     }
 
-    async getFirstNameInput(){
+    async getFirstNameInput() {
         const firstName = (await this.rl.question("Please enter your first name: ")).trim();
         return firstName;
     }
 
-    async getLastNameInput(){
+    async getLastNameInput() {
         const lastName = (await this.rl.question("Please enter your last name: ")).trim();
         return lastName
     }
 
-    async getUsernameInput(){
+    async getUsernameInput() {
         const username = (await this.rl.question("Please enter a username: ")).trim();
         return username;
     }
 
-    async getPasswordInput(){
+    async getPasswordInput() {
         const passwordInput = (await this.rl.question("Please enter a password: ")).trim();
         return passwordInput;
     }
 
-    
 
-   closeInput(){
+    async getDepositInput() {
+        let depositInput = NaN;
+
+        while (Number.isNaN(depositInput)) {
+            depositInput = parseInt((await this.rl.question("Enter a deposit amount: ")).trim());
+
+            if (Number.isNaN(depositInput)) {
+                console.log("Invalid selection: Please enter a number")
+            }
+        }
+
+        return depositInput;
+    }
+
+
+    async getWithdrawInput() {
+        let withdrawInput = NaN;
+
+        while (Number.isNaN(withdrawInput)) {
+            withdrawInput = parseInt((await this.rl.question("Enter a withdraw amount: ")).trim());
+
+            if (Number.isNaN(withdrawInput)) {
+                console.log("Invalid selection: Please enter a number")
+            }
+        }
+
+        return withdrawInput;
+    }
+
+
+    async getValueInput(type: string) {
+        type = type.trim().toUpperCase();
+        if(type !== "DEPOSIT" && type !== "WITHDRAW"){
+            console.log("Invalid selection type: ")
+            return 0;
+        }
+
+        let valueInput = NaN;
+        let typeOfTransaction = type === "DEPOSIT" ? "deposit" : "withdraw"
+        while (Number.isNaN(valueInput)) {
+            valueInput = parseInt((await this.rl.question(`Enter a ${typeOfTransaction} amount: `)).trim());
+
+            if (Number.isNaN(valueInput)) {
+                console.log("Invalid selection: Please enter a number")
+            }
+        }
+
+        return valueInput;
+    }
+    // only numbers for account and balances
+
+    closeInput() {
         this.rl.close()
     }
 
 
 }
 
-    
-    // 
+
+
 
 async function register() {
     console.log("*** Register ***")
     const newInput = new Input();
-    const firstname = await newInput.getFirstNameInput();
-    const lastname = await newInput.getLastNameInput();
-    const username = await newInput.getUsernameInput();
-    const password = await newInput.getPasswordInput();
-    console.log(firstname)
-    console.log(lastname)
-    console.log(username)
-    console.log(password)
-
+    const deposit = await newInput.getValueInput("deposit")
+    console.log(deposit)
 
 }
 
